683d2597c7ce36300455c5c40397d62b204934fa
samir mahmud <samir93bj@gmail.com>
2023-03-25 18:39:15 -0300

    feat: initialize config graphql

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,16 +4,15 @@ const { config } = require('../config/config');
 const setupModels = require('./models');
 
 const options = {
-  dialect: 'postgres',
-  logging: config.isDev ? console.log : false,
-}
+  dialect: 'postgres'
+};
 
 if (config.isProd) {
   options.dialectOptions = {
     ssl: {
       rejectUnauthorized: false
     }
-  }
+  };
 }
 
 const sequelize = new Sequelize(config.dbUrl, options);
